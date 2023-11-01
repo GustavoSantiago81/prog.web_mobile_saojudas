6ad60138dd4e960958b0393d2de4fe326047acfc
Gustavo Santiago Pereira - 822164746 <822164746@ulife.com.br>
2023-10-31 21:35:15 -0300

    express e nodemon

--- Noriega/http sera 17_10/http/index.js
+++ Noriega/http sera 17_10/http/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.get("/", function(req,res){
+    res.send("<h1>Bem vindo</h1>");
+});
+
+app.get("/blog", function(req,res){
+    res.send("<h1>Bem vindo ao blog</h1>");
+});
+
+app.get("/canal/youtube", function(req,res){
+    res.send("<h1>Bem vindo ao canal</h1>");
+});
+
 app.listen(4000,(erro => (erro ? 
     console.log("Ocorreu um erro") : console.log("Servidor iniciado com sucesso!"))))
